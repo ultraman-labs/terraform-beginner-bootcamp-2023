1ba18b665e1c80af70987b780f909680764e681e
Ultra-Man <59581412+ultraman-labs@users.noreply.github.com>
2023-09-28 16:53:38 -0700

    21 restructure root module (#23)
    * 19 wip
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Create .keep
    
    * Create week0
    
    * Update week0
    
    * Update README.md
    
    * Rename week0 to week0.md
    
    * Update week0.md
    
    * Update week0.md
    
    * Update week0.md
    
    * Update week0.md
    
    * Update week0.md
    
    * Update week0.md
    
    * Update week0.md
    
    * Update week0.md
    
    * Update week0.md
    
    * Update week0.md
    
    * Update week0.md
    
    * Update week0.md
    
    * Create week1.md
    
    * Update README.md
    
    * Update week1.md
    
    * #19 change structure of root module and add tagging to bucket

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "user_uuid" {
+  description = "UUID for the user"
+  
+  type        = string
+  validation {
+    condition     = can(regex("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$", var.user_uuid))
+    error_message = "Invalid user UUID format. Please use a valid UUID."
+   }
+}
+
