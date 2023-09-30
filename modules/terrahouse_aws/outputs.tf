0eb69d998b04b5eb6c385a2f87229538c593c0d3
Ultra-Man <techfixer@live.com>
2023-09-30 05:45:38 +0000

    #28 enabled  static website hosting, uploaded index and error file via terraform

--- modules/terrahouse_aws/outputs.tf
+++ modules/terrahouse_aws/outputs.tf
@@ -1,3 +1,9 @@
-output "bucket_name" {
+  output "bucket_name" {
   value = aws_s3_bucket.website_bucket
+}
+
+output "website_endpoint" {
+  value = aws_s3_bucket_website_configuration.website_configuration.website_endpoint
+
+  
 }
