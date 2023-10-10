4aceb6b12c21aa06f489a49a871a47f848debcf7
Ultra-Man <techfixer@live.com>
2023-10-10 03:09:58 +0000

    #48 test our terra home on terratowns test town.

--- main.tf
+++ main.tf
@@ -22,29 +22,31 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567/api"
-  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
-  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  endpoint = var.terratowns_endpoint
+  user_uuid = var.teacherseat_user_uuid
+  token = var.terratowns_access_token
 }
 
-# module "terrahouse_aws" {
-  # source = "./modules/terrahouse_aws"
-  # user_uuid = var.user_uuid
-  # bucket_name = var.bucket_name
-  # index_html_filepath = var.index_html_filepath
-  # error_html_filepath = var.error_html_filepath
-  # content_version = var.content_version
-# }
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.teacherseat_user_uuid
+  bucket_name = var.bucket_name
+  index_html_filepath = var.index_html_filepath
+  error_html_filepath = var.error_html_filepath
+  content_version = var.content_version
+  assets_path = var.assets_path
+}
 
 resource "terratowns_home" "home" {
-  name = "Ultraman Comics"
+  name = "Comics"
   description = <<DESCRIPTION
 EPISODE 1.0.0: Ultraman finds himself protecting Terra-Towns from a great foe.
 Knowing he needs help...yet feeling alone, he may not be able
 to beat this monster who is bent on destroying anything in its path.
 DESCRIPTION
-  #domain_name = module.terrahouse_aws.cloudfront_url
-  domain_name = "3fdq3gz.cloudfront.net"
-  town = "gamers-grotto"
+  domain_name = module.terrahouse_aws.cloudfront_url
+  #domain_name = "3fafa3.cloudfront.net"
+  #domain_name = "module.terrahouse_aws.cloudfront.net"
+  town = "missingo"
   content_version = 1
 }
